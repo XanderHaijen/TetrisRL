86e4f6f3b168efc9adbffed683b47b4b4e7abc30
XanderHaijen <xander.haijen@student.kuleuven.be>
2021-09-21 11:10:27 +0200

    Fix in value function + general cleanup

--- Evaluation/Evaluate_policy.py
+++ Evaluation/Evaluate_policy.py
@@ -1,19 +1,17 @@
 import time
-
+import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-
 from Models.Model import Model
 from gym import Env
 
 
-def evaluate_policy(model: Model, env: Env, nb_episodes: int, render: bool = False) -> pd.DataFrame():
+def evaluate_policy(algorithm: Model, env: Env, nb_episodes: int, render: bool = False) -> pd.DataFrame():
     """
 
     :param render: if True, the games will be shown on screen
-    :param model: of type Model: provides the policy to follow
-    :param env: the environment in which to test the provided :param model
-    :param nb_episodes: number of evaluation epsiodes
+    :param algorithm: of type Model: provides the policy to follow
+    :param env: the environment in which to test the provided :param algorithm
+    :param nb_episodes: number of evaluation episodes
     :return: returns the mean and variance of number of pieces placed, number of lines cleared and score achieved
     """
 
@@ -33,12 +31,11 @@ def evaluate_policy(model: Model, env: Env, nb_episodes: int, render: bool = Fal
             if render:  # only for viewing purposes
                 env.render()
                 time.sleep(0.05)
-            actions = model.predict(state)
-            for action in actions:
-                state, reward, done, data = env.step(action)
-                if data["new_piece"]:
-                    nb_pieces += 1
-                total_cleared += data["lines_cleared"]
+            action = algorithm.predict(state)
+            state, reward, done, data = env.step(action)
+            if data["new_piece"]:
+                nb_pieces += 1
+            total_cleared += data["lines_cleared"]
 
         lines_cleared = data.get("lines_cleared", 0)
         score = data.get("score", 0)
@@ -48,5 +45,19 @@ def evaluate_policy(model: Model, env: Env, nb_episodes: int, render: bool = Fal
     return metrics_df
 
 
-
-
+def plot_with_errors(x_sequence, y_sequence, name, image_path, fig_nb) -> None:
+    """
+    :param name: name to put on the y-axis and in the label of the legend
+    :param fig_nb: ensures plots are not overwritten
+    :param x_sequence: list containing moments of measure (in number of episodes)
+    :param y_sequence: list or tuple containing a number of pairs (mean, variance)
+    :param image_path: path where the figure is saved
+    :return: None
+    """
+    plt.figure(fig_nb)
+    plt.errorbar(x_sequence, [mean for mean, _ in y_sequence], [var for _, var in y_sequence],
+                 linestyle='-', marker='^', label=name)
+    plt.legend()
+    plt.xlabel("Number of episodes trained")
+    plt.ylabel(name)
+    plt.savefig(image_path)
